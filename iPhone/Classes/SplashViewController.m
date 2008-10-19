a1bcf2d3cad9188c96c6568d6af5bc2a0b647d7c
Aubrey Francois <aubrey@aubreyfrancois.com>
2008-10-19 00:43:38 -0600

    Added some missed files.

---
+++ iPhone/Classes/SplashViewController.m
@@ -0,0 +1,63 @@
+//
+//  SplashViewController.m
+//  iPhone
+//
+//  Created by Aubrey Francois on 10/18/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "SplashViewController.h"
+
+
+@implementation SplashViewController
+@synthesize delegate;
+
+/*
+// Override initWithNibName:bundle: to load the view using a nib file then perform additional customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically.
+- (void)loadView {
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+- (IBAction)splashOk:(id)sender forEvent:(UIEvent*)event
+{
+	if([delegate respondsToSelector:@selector(splashDidDoOk)])
+	{
+		[delegate performSelector:@selector(splashDidDoOk) withObject:nil afterDelay:0];
+	}
+}
+
+@end
