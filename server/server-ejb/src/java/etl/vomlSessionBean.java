b6394d37b1a29a0046b9e6e3cb45dd0460ff806a
Dave Angulo <daveangulo@gmail.com>
2008-11-06 15:38:44 -0700

    First cut at adding in service for uploading voml data.
    server
      + modified vomlSessionBean to map attribs to our data
      + added a admin web frontend to run the load data command

--- server/server-ejb/src/java/etl/vomlSessionBean.java
+++ server/server-ejb/src/java/etl/vomlSessionBean.java
@@ -6,6 +6,10 @@
 package etl;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.NoResultException;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -15,7 +19,12 @@ import javax.xml.bind.ValidationEvent;
 import javax.xml.bind.util.ValidationEventCollector;
 import java.io.File;
 import java.util.List;
+import java.util.Date;
+import java.util.UUID;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import org.networkforgood.xml.namespaces.voml.*;
+import persistence.*;
 
 /**
  *
@@ -23,6 +32,8 @@ import org.networkforgood.xml.namespaces.voml.*;
  */
 @Stateless
 public class vomlSessionBean implements vomlSessionLocal {
+    @PersistenceContext
+    private EntityManager em;
 
     public void loadVoml() {
         try {
@@ -32,9 +43,94 @@ public class vomlSessionBean implements vomlSessionLocal {
             vo = (VolunteerOpportunities) unmarshaller.unmarshal(new File("/Users/dave/Documents/iVolunteer/code/ivolunteer/test_data/handsonnetwork_restricted_mucked.xml"));
             List<VolunteerOpportunity> opps = vo.getVolunteerOpportunity();
 
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Query organizationQuery = em.createNamedQuery("Organizations.findByName");
+            Query eventQuery = em.createNamedQuery("Events.findByTitle");
             for ( VolunteerOpportunity opp : opps )
             {
                 System.out.println(opp.getTitle());
+
+                SponsoringOrganization sponsor = opp.getSponsoringOrganizations().getSponsoringOrganization().iterator().next();
+                organizationQuery.setParameter("name", sponsor.getName());
+
+                Organizations org;
+                boolean newOrg = false;
+                try {
+                    org = (Organizations) organizationQuery.getSingleResult();
+                } catch (NoResultException nr) {
+                    newOrg = true;
+                    org = new Organizations();
+                    org.setId(UUID.randomUUID().toString());
+                    org.setName(sponsor.getName());
+                }
+
+                String sponsorAddress = sponsor.getAddress1() + " " + sponsor.getAddress2();
+                if (! org.getStreet().equalsIgnoreCase(sponsorAddress)) {
+                    org.setStreet(sponsorAddress);
+                }
+
+                if (! org.getCity().equalsIgnoreCase(sponsor.getCity())) {
+                    org.setCity(sponsor.getCity());
+                }
+
+                if (! org.getState().equalsIgnoreCase(sponsor.getStateOrProvince())) {
+                    org.setState(sponsor.getStateOrProvince());
+                }
+
+                if (! org.getZip().equalsIgnoreCase(sponsor.getZipOrPostalCode())) {
+                    org.setZip(sponsor.getZipOrPostalCode());
+                }
+
+                if (! org.getDescription().equalsIgnoreCase(sponsor.getDescription())) {
+                    org.setDescription(sponsor.getDescription());
+                }
+
+                if (org.getEmail().compareTo(sponsor.getEmail()) != 0) {
+                    org.setEmail(sponsor.getEmail());
+                }
+
+                if (org.getUrl().compareTo(sponsor.getURL()) != 0) {
+                    org.setUrl(sponsor.getURL());
+                }
+
+                String sponsorPhone = sponsor.getPhone();
+                if (sponsor.getExtension() != null) {
+                    sponsorPhone = sponsorPhone + " ext " + sponsor.getExtension();
+                }
+
+                if (! org.getPhone().equalsIgnoreCase(sponsorPhone)) {
+                    org.setPhone(sponsorPhone);
+                }
+
+
+                Events ev = new Events();
+                ev.setTitle(opp.getTitle());
+                ev.setDescription(opp.getDescription());
+                ev.setStreet(opp.getLocations().getLocation().getAddress1() + " " + opp.getLocations().getLocation().getAddress2());
+                ev.setCity(opp.getLocations().getLocation().getCity());
+                ev.setState(opp.getLocations().getLocation().getStateOrProvince());
+                ev.setZip(opp.getLocations().getLocation().getZipOrPostalCode());
+                OpportunityDate oppDate = opp.getOpportunityDates().getOpportunityDate().iterator().next();
+
+                try {
+                    Date startDate = dateFormatter.parse(oppDate.getStartDate() + " " + oppDate.getStartTime());
+                    ev.setTimestamp(startDate);
+                    if ( oppDate.getDuration() != null ) {
+                        String durUnits = oppDate.getDuration().getDurationUnit();
+
+
+                    }
+                    else
+                    {
+                        Date endDate = dateFormatter.parse(oppDate.getEndDate() + " " + oppDate.getEndTime());
+                        long dur = (endDate.getTime() - startDate.getTime())/1000;
+                        ev.setDuration((short) dur);
+                    }
+                }
+                catch (ParseException pe) {
+                    System.out.println(pe.toString());
+                }
+
             }
         }
         catch( UnmarshalException ue ) {
@@ -45,6 +141,10 @@ public class vomlSessionBean implements vomlSessionLocal {
             je.printStackTrace();
         }
 
+    }
+
+    public void persist(Object object) {
+        em.persist(object);
     }
 
     
